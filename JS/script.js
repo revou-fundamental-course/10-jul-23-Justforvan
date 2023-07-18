a76bc1a03e3fa2982613e5b2cf5af74c8813bb0a
Justforvan <justforvangroup@gmail.com>
2023-07-18 16:20:47 +0800

    Masukkan fungsi untuk memberikan penjelasan dan penambahan validasi form

--- JS/script.js
+++ JS/script.js
@@ -23,11 +23,24 @@ function submitForm(event) {
       };
 
     }
+
+    function keteranganIMT() {
+      if (ukurIMT < 18.40) {
+        return "Peringatan! Kekurangan berat badan dapat berdampak serius pada kesehatan Anda. Segera ambil langkah untuk meningkatkan berat badan secara sehat. Konsultasikan dengan profesional kesehatan dan fokus pada pola makan seimbang dan olahraga yang tepat. Prioritaskan kesehatan Anda untuk mencapai berat badan yang sehat dan kesejahteraan yang optimal.";
+      } else if (ukurIMT >= 18.40 && ukurIMT <= 25.00) {
+        return "Selamat! Berat badan normal Anda adalah prestasi luar biasa. Anda telah menunjukkan komitmen terhadap kesehatan dan kesejahteraan pribadi. Teruslah menjadi inspirasi bagi orang lain dan jadikan hidup sehat sebagai gaya hidup yang berkelanjutan. Selamat atas pencapaian Anda yang luar biasa!";
+      } else {
+        return "Perhatian! Kelebihan berat badan bisa berisiko serius bagi kesehatan. Penting untuk segera mengambil tindakan untuk menurunkan berat badan. Pilih pola makan sehat dan tingkatkan aktivitas fisik untuk mencapai berat badan yang ideal dan mengurangi risiko kesehatan. Jaga kesehatan Anda dengan langkah-langkah kecil yang konsisten.";
+      }
+    }
+    
+    
     
 
 
     let hasilDiv = document.getElementById('hasil');
     hasilDiv.innerHTML = '<h3>IMT Anda: </h3><p> <strong>' + ukurIMT + '</strong>, termasuk kategori <strong>' + cekKategoriIMT().toUpperCase() + '</strong></p>' +
+                        '<p>' + keteranganIMT() + '</p>' + 
                         '<button id="reset-ukuran">Ukur Ulang</button>';
   
     // Add an event listener to the "Ukur" button
